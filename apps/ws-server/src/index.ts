559f27fdd727b1545a1b3cade3cb7f9ac45d2b1c
Rachit Gandhi <rachitgandhi138@gmail.com>
2026-06-05 14:40:57 +0530

    Configured boilerplate of http and ws server

---
+++ apps/ws-server/src/index.ts
@@ -0,0 +1,17 @@
+import { Socket } from "socket.io";
+import { WebSocketServer } from "ws";
+import {client} from "@repo/db/client";
+
+const server = new WebSocketServer({
+    port:3001
+});
+
+server.on ("connection",(socket)=>{
+    client.user.create({
+        data:{
+            username: Math.random().toString(),
+            password: Math.random().toString()
+        }
+    })
+    socket.send("HI there, you are connected on server");
+})
