9193f7f59ea92a2223e521e2f90ef0df376b7d4c
Rachit Gandhi <rachitgandhi138@gmail.com>
2026-06-06 14:17:34 +0530

    Completed the app v1

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,17 +1,23 @@
-import { Socket } from "socket.io";
+import "dotenv/config";
 import { WebSocketServer } from "ws";
-import {client} from "@repo/db/client";
+import { client } from "@repo/db/client";
 
 const server = new WebSocketServer({
-    port:3001
+    port: 3001
 });
 
-server.on ("connection",(socket)=>{
-    client.user.create({
-        data:{
-            username: Math.random().toString(),
-            password: Math.random().toString()
-        }
-    })
-    socket.send("HI there, you are connected on server");
-})
+server.on("connection", async (socket) => {
+    try {
+        client.user.create({
+            data: {
+                username: Math.random().toString(),
+                password: Math.random().toString()
+            }
+        });
+       
+        socket.send("HI there, you are connected on server");
+    } catch (err) {
+        console.error("DB error:", err);
+        socket.send("Error creating user");
+    }
+});
